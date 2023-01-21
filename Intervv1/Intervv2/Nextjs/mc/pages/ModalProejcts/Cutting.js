0063e12016f9347dc6fff36c8e932788415586fc
mariana.ceballos001 <mariana.ceballos001@mymdc.net>
2023-01-21 13:47:27 -0500

    initial

---
+++ Intervv1/Intervv2/Nextjs/mc/pages/ModalProejcts/Cutting.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Cutting from "../../public/images/CuttingProject.png";
+import { useState, Component } from "react";
+import Image from "next/image";
+
+// reactstrap components
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+
+function Example() {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  return (
+    <>
+      <Button
+        className="button2"
+        type="button"
+        onClick={() => setModalOpen(!modalOpen)}
+      >
+        Open Modal{" "}
+      </Button>
+      <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
+        <div className=" modal-header">
+          <h5 className=" modal-title" id="exampleModalLabel">
+          Mathlab Modeling for Device Development{" "}
+          </h5>
+
+          <button
+            aria-label="Close"
+            className=" close"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <ModalBody>
+          {" "}
+          <div>
+            <Image src={Cutting} alt="Picture of the author" width="800x" />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="secondary"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            Close
+          </Button>
+        
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+}
+
+export default Example;
